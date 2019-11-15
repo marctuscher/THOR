ef6e5e09161ba22a6f2a01d1eb7863ee7ec8749c
Marc Tuscher <marc.tuscher@gmail.com>
2019-11-15 07:15:23 +0100

    python module

--- benchmark/make_toolkits.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd benchmark/bench_utils/pyvotkit
-python setup.py build_ext --inplace
-cd ../../..
-
-cd benchmark/bench_utils/pysot/utils/
-python setup.py build_ext --inplace
-cd ../../../..
